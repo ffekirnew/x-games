dfbd0b9dc26dc9d360ce10ab97e6ac4fc85c13a4
ffekirnew <fikernew.birhanu@a2sv.org>
2023-08-25 00:27:07 +0300

    Reactored the games grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -12,14 +12,14 @@ interface Props {
 const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery)
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+  if (error)
+    return <Text>{error}</Text>
   
-  return (<>
-    { error && <Text>{error}</Text> }
-    <SimpleGrid spacing={6} columns={{ sm: 1, md: 2, lg: 3 }} paddingY={4}>
-      { !isLoading && data.map((game) => <GameCardContainer key={game.id}><GameCard game={game}></GameCard></GameCardContainer>)}
-      { isLoading &&  skeletons.map(skeleton => <GameCardContainer key={skeleton}><GameCardSkeleton/></GameCardContainer>)}
-    </SimpleGrid>
-  </>)
+  return <SimpleGrid spacing={6} columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} paddingY={4}>
+    { isLoading && skeletons.map(skeleton => <GameCardContainer key={skeleton}><GameCardSkeleton/></GameCardContainer>)}
+    { !isLoading && data.map((game) => <GameCardContainer key={game.id}><GameCard game={game}></GameCard></GameCardContainer>)}
+  </SimpleGrid>
 }
 
 export default GameGrid
